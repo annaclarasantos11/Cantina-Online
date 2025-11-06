788bb25d2b350f631551e06d49e7deb52d3cde9c
Anna Clara Santos Silva <annac.santos@eaportal.org>
2025-11-06 10:24:35 -0300

    Marcar página de nova senha como dinâmica

--- src/app/auth/recuperar/nova-senha/page.tsx
+++ src/app/auth/recuperar/nova-senha/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { NovaSenhaClient } from "./NovaSenhaClient";
